0b84238158eb000702a83b3984c32a9f7aea490e
Josh Glazer <joshglazer@gmail.com>
2021-01-23 21:26:53 -0500

    ng update @angular/core @angular/cli

--- src/app/components/routes/now-playing/now-playing.component.spec.ts
+++ src/app/components/routes/now-playing/now-playing.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NowPlayingComponent } from './now-playing.component';
 
@@ -6,7 +6,7 @@ describe('NowPlayingComponent', () => {
   let component: NowPlayingComponent;
   let fixture: ComponentFixture<NowPlayingComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [NowPlayingComponent],
     }).compileComponents();
